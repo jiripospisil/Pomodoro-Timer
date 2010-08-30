d58f670a4da9928728768ad06ab98afbb87db259
Jiri Pospisil <mekishizufu@gmail.com>
2010-08-30 20:25:34 +0200

    First draft.

---
+++ src/info/jiripospisil/pomodoro/actions/PomodoroAction.java
@@ -0,0 +1,34 @@
+package info.jiripospisil.pomodoro.actions;
+
+import info.jiripospisil.pomodoro.ui.PomodoroPanel;
+import info.jiripospisil.pomodoro.ui.PomodoroPanelBack;
+import java.awt.Component;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import javax.swing.JButton;
+import org.openide.util.actions.Presenter;
+
+/**
+ *
+ * @author Jiri Pospisil <mekishizufu@gmail.com>
+ */
+public class PomodoroAction implements ActionListener, Presenter.Toolbar {
+
+    private final PomodoroPanelBack back;
+    private final PomodoroPanel panel;
+
+    public PomodoroAction() {
+        panel = new PomodoroPanel();
+        back = new PomodoroPanelBack(panel);
+    }
+
+
+    @Override
+    public void actionPerformed(ActionEvent e) {
+    }
+
+    @Override
+    public Component getToolbarPresenter() {
+        return panel;
+    }
+}
---
+++ src/info/jiripospisil/pomodoro/ui/PomodoroPanelBack.java
@@ -0,0 +1,97 @@
+package info.jiripospisil.pomodoro.ui;
+
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import javax.swing.ImageIcon;
+import javax.swing.JOptionPane;
+import javax.swing.Timer;
+import org.openide.util.ImageUtilities;
+import org.openide.util.NbBundle;
+
+public class PomodoroPanelBack {
+
+    private static final int TIME_LIMIT = 20;
+    private final PomodoroPanel panel;
+    private final Timer timer;
+    private boolean isRunning;
+    private int timeLeft;
+
+    public PomodoroPanelBack(PomodoroPanel panel) {
+        this.panel = panel;
+        this.panel.addButtonListener(new ClickListener());
+
+        this.timer = new Timer(0, new TickListener());
+        this.timer.setDelay(1000);
+
+        this.isRunning = false;
+        this.timeLeft = TIME_LIMIT;
+
+        updateButtonTime();
+    }
+
+    class ClickListener implements ActionListener {
+
+        @Override
+        public void actionPerformed(ActionEvent e) {
+            if (isRunning) {
+                stopCountdown();
+            } else {
+                startCountdown();
+            }
+        }
+    }
+
+    class TickListener implements ActionListener {
+
+        @Override
+        public void actionPerformed(ActionEvent e) {
+            timeLeft -= 1;
+
+            updateButtonTime();
+
+            if (timeLeft == 0) {
+                showNotification();
+                stopCountdown();
+            }
+        }
+    }
+
+    private void startCountdown() {
+        isRunning = true;
+
+        resetTime();
+        updateButtonTime();
+
+        timer.start();
+    }
+
+    private void stopCountdown() {
+        isRunning = false;
+
+        resetTime();
+        updateButtonTime();
+
+        timer.stop();
+    }
+
+    private void resetTime() {
+        timeLeft = TIME_LIMIT;
+    }
+
+    private void updateButtonTime() {
+        String time = String.format("%02d:%02d", timeLeft / 60, timeLeft % 60);
+        panel.getButton().setText(time);
+    }
+
+    private void showNotification() {
+        String message = NbBundle.getMessage(PomodoroPanelBack.class,
+                "PomodoroPanelBack.notification.text");
+        String title = NbBundle.getMessage(PomodoroPanelBack.class,
+                "PomodoroPanelBack.notification.title");
+
+        JOptionPane.showMessageDialog(null,
+                message, title, JOptionPane.INFORMATION_MESSAGE,
+                new ImageIcon(ImageUtilities.loadImage(
+                "info/jiripospisil/pomodoro/resources/icon.gif")));
+    }
+}
